9084a9a75cea5a26a992ca32f4831f7b64ad6c43
AnViSe <anvise@grodno.belpost.by>
2021-08-31 15:40:52 +0300

    Change urls

--- bp/config/urls.py
+++ bp/config/urls.py
@@ -1,4 +1,4 @@
-"""bp URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
+    # path('api/v1/', include('config.urls_api_v1')),
+    # path('refs/', include('references.urls')),
 ]
