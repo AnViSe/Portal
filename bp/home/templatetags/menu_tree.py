90b7275365db6da480a02c96a6ddb70e0c46a9fd
AnViSe <anvise@grodno.belpost.by>
2021-09-02 16:26:12 +0300

    Шаблон дерева меню

---
+++ bp/home/templatetags/menu_tree.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('tags/menu_tree.html')
+def show_menu_tree(params=None):
+    menu_items = [{"title": "Меню 1", "url": "home", "icon": "fas fa-th", "badge": "12"},
+                  {"title": "Меню 2", "url": "account_email"}, ]
+    return {"menus": menu_items}
