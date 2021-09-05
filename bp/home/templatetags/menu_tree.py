d644781e3cad262ffcc5ef1787fe3786da3e16be
AnViSe <anvise@grodno.belpost.by>
2021-09-05 23:11:15 +0300

    Доработка генерации дерева меню

--- bp/home/templatetags/menu_tree.py
+++ bp/home/templatetags/menu_tree.py
@@ -1,10 +1,14 @@
 from django import template
 
+from ..models import Menu
+
 register = template.Library()
 
 
 @register.inclusion_tag('tags/menu_tree.html')
-def show_menu_tree(params=None):
-    menu_items = [{"title": "Меню 1", "url": "home", "icon": "fas fa-th", "badge": "12"},
-                  {"title": "Меню 2", "url": "employees"}, ]
-    return {"menus": menu_items}
+def show_menu_tree(params=None, url=None):
+    # menu_items = Menu.objects.filter(status=1).order_by('sort')
+    # menu_items = [{"title": "Меню 1", "url": "home", "icon": "fas fa-th", "badge": "12"},
+    #               {"title": "Меню 2", "url": "employees"}, ]
+    menu_items = Menu.get_user_menu(user=params)
+    return {"menus": menu_items, "cur_url": url}
