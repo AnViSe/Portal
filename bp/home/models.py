20ee4d101c383ce78dca679e492ff376ee6751f7
AnViSe <anvise@grodno.belpost.by>
2021-09-05 16:44:06 +0300

    Модель Menu

--- bp/home/models.py
+++ bp/home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL,
+                               blank=True, null=True,
+                               verbose_name='Родитель')
+    name = models.CharField(max_length=100,
+                            verbose_name='Заголовок')
+    route = models.CharField(max_length=100,
+                             blank=True, null=True,
+                             verbose_name='Имя маршрута')
+    icon = models.CharField(max_length=30,
+                            blank=True, null=True,
+                            default='fas fa-circle',
+                            verbose_name='Иконка')
+    badge = models.CharField(max_length=20,
+                             blank=True, null=True,
+                             verbose_name='Метка')
+    sort = models.SmallIntegerField(default=999, db_index=True,
+                                    verbose_name='Сортировка')
+    status = models.SmallIntegerField(default=1, db_index=True,
+                                      verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
