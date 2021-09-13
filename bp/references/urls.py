52c2839499e812da3c0fb1fac83ac454eca8432f
AnViSe <anvise@grodno.belpost.by>
2021-09-13 17:32:39 +0300

    Корректировки шаблонов

--- bp/references/urls.py
+++ bp/references/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('employees/', login_required(EmployeeList.as_view()), name='employees'),
     path('employees/create/', login_required(EmployeeCreate.as_view()), name='add_employees'),
     path('employees/<int:pk>/', login_required(EmployeeEdit .as_view()), name='edit_employee'),
+    path('employees/<int:pk>/view/', login_required(EmployeeView .as_view()), name='view_employee'),
     path('countries/', login_required(CountryList.as_view()), name='countries'),
     path('regions/', login_required(RegionList.as_view()), name='regions'),
 ]
