f5f22e6f878ead3dc7893abe8b808998260bbdc4
AnViSe <anvise@grodno.belpost.by>
2021-10-15 17:14:36 +0300

    Приложение Модули ч.2

--- bp/apps/modules/admin.py
+++ bp/apps/modules/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.modules.models import Module
+
+admin.site.register(Module)
--- bp/apps/modules/models.py
+++ bp/apps/modules/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from core.fields import StatusField
+
+
+class Module(models.Model):
+    """Модель описания модуля"""
+
+    id = models.BigAutoField(primary_key=True,
+                             verbose_name='Идентификатор')
+    name = models.CharField(max_length=100,
+                            verbose_name='Наименование')
+    desc = models.CharField(max_length=255, blank=True, null=True,
+                            verbose_name='Описание')
+    perm = models.CharField(max_length=100, blank=True, null=True,
+                            verbose_name='Разрешение')
+    icon = models.CharField(max_length=30, blank=True, null=True,
+                            default='fas fa-microchip',
+                            verbose_name='Иконка')
+    sort = models.SmallIntegerField(default=999, db_index=True,
+                                    verbose_name='Сортировка')
+    status = StatusField()
