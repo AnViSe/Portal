e4ead467408bef13a957dc01ef880f2ac8123fca
AnViSe <anvise@grodno.belpost.by>
2021-10-26 13:21:00 +0300

    Транзакции при загрузке умолчаний

--- bp/core/management/commands/makedefdata.py
+++ bp/core/management/commands/makedefdata.py
@@ -1,12 +1,13 @@
 from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from apps.home.models import Menu
 from apps.references.models.base import FlexObject, FlexType
 
 from config import settings
-from core.fields import OBJ_TYPE_NONE, OBJ_TYPE_LOCATION, OBJ_TYPE_STREET, OBJ_TYPE_PHONE, OBJ_TYPE_ADDRESS, \
-    OBJ_TYPE_SUBDIVISION, OBJ_TYPE_GEN_IZV
+from core.fields import OBJ_TYPE_NONE, OBJ_TYPE_LOCATION, OBJ_TYPE_STREET, OBJ_TYPE_PHONE, \
+    OBJ_TYPE_ADDRESS, OBJ_TYPE_SUBDIVISION, OBJ_TYPE_GEN_IZV
 
 
 class Command(BaseCommand):
@@ -42,12 +43,10 @@ class Command(BaseCommand):
                             icon='fas fa-road')
         Menu.objects.create(title='Здания', parent=parent_menu,
                             perm='references.view_building', route='buildings',
-                            icon='fas fa-building',
-                            status=0)
+                            icon='fas fa-building')
         Menu.objects.create(title='Адреса', parent=parent_menu,
                             perm='references.view_address', route='addresses',
-                            icon='fas fa-location-dot',
-                            status=0)
+                            icon='fas fa-location-dot')
         Menu.objects.create(title='Персоны', parent=parent_menu,
                             perm='references.view_person', route='persons',
                             icon='fas fa-person')
@@ -93,127 +92,134 @@ class Command(BaseCommand):
         _count = FlexObject.objects.all().count()
         self.stdout.write(self.style.SUCCESS(f"Видов сущностей: {_count}"))
 
-        fo_pk = OBJ_TYPE_LOCATION
-        FlexType.objects.filter(type_object_id=fo_pk).delete()
-        FlexType.objects.create(type_code='1', type_name='нет', type_name_full='Не указано',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='2', type_name='г.', type_name_full='город',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='3', type_name='гп.', type_name_full='городской поселок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='4', type_name='аг.', type_name_full='агрогородок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='5', type_name='д.', type_name_full='деревня',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='6', type_name='п.', type_name_full='поселок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='7', type_name='х.', type_name_full='хутор',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='8', type_name='кп.', type_name_full='курортный поселок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='9', type_name='рп.', type_name_full='рабочий поселок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='10', type_name='с.', type_name_full='сельсовет',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='11', type_name='снп.',
-                                type_name_full='сельский населенный пункт',
-                                type_object_id=fo_pk)
+        with transaction.atomic():
+            fo_pk = OBJ_TYPE_LOCATION
+            FlexType.objects.filter(type_object_id=fo_pk).delete()
+            FlexType.objects.create(type_code='1', type_name='нет', type_name_full='Не указано',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='2', type_name='г.', type_name_full='город',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='3', type_name='гп.',
+                                    type_name_full='городской поселок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='4', type_name='аг.', type_name_full='агрогородок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='5', type_name='д.', type_name_full='деревня',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='6', type_name='п.', type_name_full='поселок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='7', type_name='х.', type_name_full='хутор',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='8', type_name='кп.',
+                                    type_name_full='курортный поселок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='9', type_name='рп.',
+                                    type_name_full='рабочий поселок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='10', type_name='с.', type_name_full='сельсовет',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='11', type_name='снп.',
+                                    type_name_full='сельский населенный пункт',
+                                    type_object_id=fo_pk)
 
-        fo_pk = OBJ_TYPE_STREET
-        FlexType.objects.filter(type_object_id=fo_pk).delete()
-        FlexType.objects.create(type_code='12', type_name='б-р', type_name_full='бульвар',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='13', type_name='вг', type_name_full='военный городок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='14', type_name='вч', type_name_full='военная часть',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='15', type_name='въезд', type_name_full='въезд',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='16', type_name='кв-л', type_name_full='квартал',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='17', type_name='мкр-н', type_name_full='микрорайон',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='18', type_name='наб.', type_name_full='набережная',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='19', type_name='пер.', type_name_full='переулок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='20', type_name='пл.', type_name_full='площадь',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='21', type_name='пос.', type_name_full='поселок',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='22', type_name='пр.', type_name_full='проспект',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='23', type_name='пр-д', type_name_full='проезд',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='24', type_name='ст.', type_name_full='станция',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='25', type_name='тер.', type_name_full='территория',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='26', type_name='тр.', type_name_full='тракт',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='27', type_name='туп.', type_name_full='тупик',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='28', type_name='ул.', type_name_full='улица',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='29', type_name='уроч.', type_name_full='урочище',
-                                type_object_id=fo_pk)
-        FlexType.objects.create(type_code='30', type_name='шоссе', type_name_full='шоссе',
-                                type_object_id=fo_pk)
+            fo_pk = OBJ_TYPE_STREET
+            FlexType.objects.filter(type_object_id=fo_pk).delete()
+            FlexType.objects.create(type_code='12', type_name='б-р', type_name_full='бульвар',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='13', type_name='вг',
+                                    type_name_full='военный городок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='14', type_name='вч', type_name_full='военная часть',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='15', type_name='въезд', type_name_full='въезд',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='16', type_name='кв-л', type_name_full='квартал',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='17', type_name='мкр-н', type_name_full='микрорайон',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='18', type_name='наб.', type_name_full='набережная',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='19', type_name='пер.', type_name_full='переулок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='20', type_name='пл.', type_name_full='площадь',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='21', type_name='пос.', type_name_full='поселок',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='22', type_name='пр.', type_name_full='проспект',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='23', type_name='пр-д', type_name_full='проезд',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='24', type_name='ст.', type_name_full='станция',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='25', type_name='тер.', type_name_full='территория',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='26', type_name='тр.', type_name_full='тракт',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='27', type_name='туп.', type_name_full='тупик',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='28', type_name='ул.', type_name_full='улица',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='29', type_name='уроч.', type_name_full='урочище',
+                                    type_object_id=fo_pk)
+            FlexType.objects.create(type_code='30', type_name='шоссе', type_name_full='шоссе',
+                                    type_object_id=fo_pk)
 
-        fo_pk = OBJ_TYPE_ADDRESS
-        FlexType.objects.filter(type_object_id=fo_pk).delete()
-        FlexType.objects.create(type_code='0', type_name='Не указан', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='1', type_name='Доставочный', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='2', type_name='Домашний', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='3', type_name='Рабочий', type_object_id=fo_pk)
+            fo_pk = OBJ_TYPE_ADDRESS
+            FlexType.objects.filter(type_object_id=fo_pk).delete()
+            FlexType.objects.create(type_code='0', type_name='Не указан', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='1', type_name='Доставочный', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='2', type_name='Домашний', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='3', type_name='Рабочий', type_object_id=fo_pk)
 
-        fo_pk = OBJ_TYPE_PHONE
-        FlexType.objects.filter(type_object_id=fo_pk).delete()
-        FlexType.objects.create(type_code='0', type_name='Не указан', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='1', type_name='Мобильный', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='2', type_name='Домашний', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='3', type_name='Рабочий', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='4', type_name='Внутренний', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='5', type_name='Виртуальный', type_object_id=fo_pk)
+            fo_pk = OBJ_TYPE_PHONE
+            FlexType.objects.filter(type_object_id=fo_pk).delete()
+            FlexType.objects.create(type_code='0', type_name='Не указан', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='1', type_name='Мобильный', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='2', type_name='Домашний', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='3', type_name='Рабочий', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='4', type_name='Внутренний', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='5', type_name='Виртуальный', type_object_id=fo_pk)
 
-        fo_pk = OBJ_TYPE_SUBDIVISION
-        FlexType.objects.filter(type_object_id=fo_pk).delete()
-        FlexType.objects.create(type_code='0', type_name='Не указан', type_object_id=fo_pk)
-        FlexType.objects.create(type_code='1', type_name='РУП', type_object_id=fo_pk,
-                                type_name_full='Республиканское унитарное предприятие')
-        FlexType.objects.create(type_code='2', type_name='Филиал', type_object_id=fo_pk,
-                                type_name_full='Областной филиал')
-        FlexType.objects.create(type_code='6', type_name='Цех', type_object_id=fo_pk,
-                                type_name_full='Цех')
-        FlexType.objects.create(type_code='3', type_name='РУПС', type_object_id=fo_pk,
-                                type_name_full='Районный узел почтовой связи')
-        FlexType.objects.create(type_code='4', type_name='УПС', type_object_id=fo_pk,
-                                type_name_full='Участок почтовой связи')
-        FlexType.objects.create(type_code='5', type_name='ОПС', type_object_id=fo_pk,
-                                type_name_full='Отделение почтовой связи')
-        FlexType.objects.create(type_code='61', type_name='ПС', type_object_id=fo_pk,
-                                type_name_full='Пункт связи')
-        FlexType.objects.create(type_code='62', type_name='ППС', type_object_id=fo_pk,
-                                type_name_full='Передвижной Пункт Связи')
-        FlexType.objects.create(type_code='63', type_name='ПОПС', type_object_id=fo_pk,
-                                type_name_full='Передвижное Отделение Почтовой Связи')
-        FlexType.objects.create(type_code='21', type_name='Отдел', type_object_id=fo_pk,
-                                type_name_full='', status=0)
-        FlexType.objects.create(type_code='22', type_name='Сектор', type_object_id=fo_pk,
-                                type_name_full='', status=0)
+            fo_pk = OBJ_TYPE_SUBDIVISION
+            FlexType.objects.filter(type_object_id=fo_pk).delete()
+            FlexType.objects.create(type_code='0', type_name='Не указан', type_object_id=fo_pk)
+            FlexType.objects.create(type_code='1', type_name='РУП', type_object_id=fo_pk,
+                                    type_name_full='Республиканское унитарное предприятие')
+            FlexType.objects.create(type_code='2', type_name='Филиал', type_object_id=fo_pk,
+                                    type_name_full='Областной филиал')
+            FlexType.objects.create(type_code='6', type_name='Цех', type_object_id=fo_pk,
+                                    type_name_full='Цех')
+            FlexType.objects.create(type_code='3', type_name='РУПС', type_object_id=fo_pk,
+                                    type_name_full='Районный узел почтовой связи')
+            FlexType.objects.create(type_code='4', type_name='УПС', type_object_id=fo_pk,
+                                    type_name_full='Участок почтовой связи')
+            FlexType.objects.create(type_code='5', type_name='ОПС', type_object_id=fo_pk,
+                                    type_name_full='Отделение почтовой связи')
+            FlexType.objects.create(type_code='61', type_name='ПС', type_object_id=fo_pk,
+                                    type_name_full='Пункт связи')
+            FlexType.objects.create(type_code='62', type_name='ППС', type_object_id=fo_pk,
+                                    type_name_full='Передвижной Пункт Связи')
+            FlexType.objects.create(type_code='63', type_name='ПОПС', type_object_id=fo_pk,
+                                    type_name_full='Передвижное Отделение Почтовой Связи')
+            FlexType.objects.create(type_code='21', type_name='Отдел', type_object_id=fo_pk,
+                                    type_name_full='', status=0)
+            FlexType.objects.create(type_code='22', type_name='Сектор', type_object_id=fo_pk,
+                                    type_name_full='', status=0)
 
-        fo_pk = OBJ_TYPE_GEN_IZV
-        FlexType.objects.filter(type_object_id=fo_pk).delete()
-        FlexType.objects.create(type_code='0', type_name='Сквозная нумерация', type_object_id=fo_pk,
-                                type_value='0')
-        FlexType.objects.create(type_code='1', type_name='Каждый месяц', type_object_id=fo_pk,
-                                type_value='1')
-        FlexType.objects.create(type_code='2', type_name='Каждый квартал', type_object_id=fo_pk,
-                                type_value='3')
-        FlexType.objects.create(type_code='3', type_name='Каждое полугодие', type_object_id=fo_pk,
-                                type_value='6')
-        FlexType.objects.create(type_code='4', type_name='Каждый год', type_object_id=fo_pk,
-                                type_value='12')
+            fo_pk = OBJ_TYPE_GEN_IZV
+            FlexType.objects.filter(type_object_id=fo_pk).delete()
+            FlexType.objects.create(type_code='0', type_name='Сквозная нумерация',
+                                    type_object_id=fo_pk,
+                                    type_value='0')
+            FlexType.objects.create(type_code='1', type_name='Каждый месяц', type_object_id=fo_pk,
+                                    type_value='1')
+            FlexType.objects.create(type_code='2', type_name='Каждый квартал', type_object_id=fo_pk,
+                                    type_value='3')
+            FlexType.objects.create(type_code='3', type_name='Каждое полугодие',
+                                    type_object_id=fo_pk,
+                                    type_value='6')
+            FlexType.objects.create(type_code='4', type_name='Каждый год', type_object_id=fo_pk,
+                                    type_value='12')
 
         _count = FlexType.objects.all().count()
         self.stdout.write(self.style.SUCCESS(f"Типов сущностей: {_count}"))
