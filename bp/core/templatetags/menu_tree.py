bb8f76898922faf0593b792f18c82ced6608f41b
AnViSe <anvise@grodno.belpost.by>
2021-10-29 11:34:51 +0300

    Кеширование меню

--- bp/core/templatetags/menu_tree.py
+++ bp/core/templatetags/menu_tree.py
@@ -16,13 +16,24 @@ register = template.Library()
 def get_menu_tree(cur_user=None, cur_url=None):
     perms = {None}
     if cur_user:
-        cached_perms = cache.get(f'user_perms_{cur_user.id}')
+        cached_perms = cache.get(f'perms_user_{cur_user.id}')
         if cached_perms is None:
             perms |= cur_user.get_all_permissions()
-            cache.set(f'user_perms_{cur_user.id}', perms)
+            cache.set(f'perms_user_{cur_user.id}', perms)
         else:
             perms = cached_perms
 
-    menu_items = Menu.objects.filter(level=0)
-    menus = menu_items.get_descendants(include_self=True).filter(status=1)
-    return {'menus': menus, 'cur_url': cur_url, 'perms': perms}
+    cached_menus = cache.get('menu_items')
+    if cached_menus is None:
+        menu_main = Menu.objects.filter(level=0)
+        menu_items = menu_main.get_descendants(include_self=True).filter(status=1)
+        cache.set('menu_items', menu_items)
+    else:
+        menu_items = cached_menus
+
+    menus = []
+    for menu in menu_items:
+        if menu.perm is None or menu.perm in perms:
+            menus.append(menu)
+
+    return {'menus': menus, 'cur_url': cur_url}
