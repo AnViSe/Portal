3c6e1c3eab5d7369f342375507e21c52e1758a16
AnViSe <anvise@grodno.belpost.by>
2021-11-03 17:17:49 +0300

    Видимость меню Модуль от прав ч.2

--- bp/apps/modules/views.py
+++ bp/apps/modules/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import ListView
 
 from apps.modules.models import Module
 
 
-class ModulesMainView(ListView):
+class ModulesMainView(PermissionRequiredMixin, ListView):
+    """Просмотр списка модулей"""
+
+    permission_required = 'modules.view_module'
+
     model = Module
     template_name = 'modules/index.html'
     context_object_name = 'mods_list'
