2363cd15ccaaf9c06eec58af0e903c13d3dfd92d
AnViSe <anvise@grodno.belpost.by>
2021-11-04 19:07:29 +0300

    Убрал debug_toolbar.middleware

--- bp/config/settings.py
+++ bp/config/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'maintenancemode.middleware.MaintenanceModeMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 # MIDDLEWARE_CLASSES = [
