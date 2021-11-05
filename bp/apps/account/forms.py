688a365d73b8c4c3eb76bf913e9ef844e5a1b214
AnViSe <anvise@grodno.belpost.by>
2021-11-05 14:16:01 +0300

    Чистка кода

--- bp/apps/account/forms.py
+++ bp/apps/account/forms.py
@@ -1,42 +1,15 @@
 from django import forms
-from django.urls import reverse_lazy
-from django_select2 import forms as s2forms
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import Group
 
-from apps.references.models.subdivision import Subdivision
-from extensions.widgets import BaseSelect2Widget, SubdivisionWidget
 from apps.account.models import CustomUser
-# from apps.references.models.employee import Employee
-# from apps.references.models.subdivision import Subdivision
-
-
-# class EmployeeWidget(BaseSelect2Widget):
-#     empty_label = '-- Выберите сотрудника --'
-#     search_fields = [
-#         'tab_num__icontains',
-#         'person__name_lfm__icontains',
-#     ]
-#     queryset = Employee.objects.select_related('person').all().order_by('person__name_lfm')
-
-
-# class SubdivisionWidget(BaseSelect2Widget):
-#     empty_label = '-- Выберите подразделение --'
-#     search_fields = ['name__icontains',]
-#     queryset = Subdivision.objects.all().order_by('name')
 
 
 class CustomUserCreationForm(UserCreationForm):
 
-    # subdivision = forms.ModelChoiceField(
-    #     widget=SubdivisionWidget(
-    #         queryset=Subdivision.objects.all().order_by('name_sd'))
-    # )
-
     class Meta:
         model = CustomUser
-        # fields = ('subdivision',)
         exclude = []
 
 
@@ -46,18 +19,6 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         exclude = []
-        # widgets = {
-        #     'employee': EmployeeWidget,
-        #     'subdivision': SubdivisionWidget,
-        # }
-
-    # class Media:
-    #     js = ('','')
-
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    # self.fields['employee'].queryset = Employee.objects.none()
-    # self.fields['subdivision'].queryset = Subdivision.objects.none()
 
 
 class CustomGroupAdminForm(forms.ModelForm):
