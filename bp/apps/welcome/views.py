7908dc0efc6a95aa61cbb5c44b2706bc2e6f8a34
AnViSe <anvise@grodno.belpost.by>
2021-11-10 22:07:50 +0300

    Лишние маршруты

--- bp/apps/welcome/views.py
+++ bp/apps/welcome/views.py
@@ -1,9 +1,2 @@
-# import logging
 from django.shortcuts import render
 
-# logger = logging.getLogger('django_')
-
-
-def index(request):
-    # logger.error('Открытие страницы...')
-    return render(request, template_name='welcome/index.html', )
